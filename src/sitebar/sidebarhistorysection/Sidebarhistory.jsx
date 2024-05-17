d3a4c9f01c2c02076013e799199f7f91a2934918
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2024-05-17 19:08:25 +0600

    video-platform-update-sidebar-page-add-sidebar-history-link

--- src/sitebar/sidebarhistorysection/Sidebarhistory.jsx
+++ src/sitebar/sidebarhistorysection/Sidebarhistory.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Sidebarnavlink from '../../component/sidebarnavlink/Sidebarnavlink';
+import { MdPlaylistPlay } from "react-icons/md";
+import { GoStopwatch } from "react-icons/go";
+import { MdOutlineWatchLater } from "react-icons/md";
+import { MdArrowForwardIos } from "react-icons/md";
+import { SlLike } from "react-icons/sl";
+import './Sidebarhistory.css'
 
 const Sidebarhistory = () => {
   return (
-    <div>
-      
+    <div className='sidebarhistory-box'>
+      <div className='users-details-box'>
+        <Link className='users-ditails' to = ''>you</Link>
+        <div className='uesr-details-icon'>
+          <MdArrowForwardIos />
+        </div>
+      </div>
+        <Sidebarnavlink icon={<MdOutlineWatchLater />} linktext='history' link=''/>
+        <Sidebarnavlink icon={<MdPlaylistPlay />} linktext='playlists' link=''/>
+        <Sidebarnavlink icon={<GoStopwatch />} linktext ='watch later'  link = ''/> 
+        <Sidebarnavlink icon={<SlLike />} linktext='likeds videos' link=''/>
     </div>
   )
 }
