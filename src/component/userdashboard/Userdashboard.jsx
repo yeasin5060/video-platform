2997c7d3384b459bec305d362247d187a729fb80
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2024-05-18 22:06:07 +0600

    video-paltform-update-userdashboard-add-some-link

--- src/component/userdashboard/Userdashboard.jsx
+++ src/component/userdashboard/Userdashboard.jsx
@@ -3,6 +3,12 @@ import Nanohead from '../../utilities/Nanohead'
 import Pera from '../../utilities/Pera'
 import profile from '../../images/profile.jpg'
 import './Userdashboard.css'
+import { Link } from 'react-router-dom'
+import Sidebarnavlink from '../sidebarnavlink/Sidebarnavlink'
+import { FaGoogle } from "react-icons/fa";
+import { MdAccountCircle } from "react-icons/md";
+import { MdLogout } from "react-icons/md";
+
 const Userdashboard = ({userdashboard}) => {
   return (
     <div className={userdashboard}>
@@ -13,10 +19,15 @@ const Userdashboard = ({userdashboard}) => {
             <div className='userdash-name-box'>
                 <Nanohead text='yeasun munshi' style='userdash-name-head'/>
                 <Pera text="yeasin.munshi@gmail.com" style="userdash-email"/>
+                <div className='user-channel-box'>
+                  <Link className= "user-create-channel" to="">Create a channel</Link>
+                </div>
             </div>
         </div>
         <div className='userdash-all-link-box'>
-
+          <Sidebarnavlink icon={<FaGoogle />} linktext='google' link=''/>
+          <Sidebarnavlink icon={<MdAccountCircle />} linktext='Switch account' link=''/>
+          <Sidebarnavlink icon={<MdLogout />} linktext='Sign out' link=''/>
         </div>
     </div>
   )
